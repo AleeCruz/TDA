2d64c95a073063ea95ef12ce4e9e60f9d10c8a51
Sabrina GL <sa.garcialucentini@gmail.com>
2025-09-10 19:35:10 -0300

    Se agrego algoritmo greedy para organizar batallas y pruebas

---
+++ Trabajo_Practico_1/pruebas.py
@@ -0,0 +1,68 @@
+from Trabajo_Practico import organizar_batallas
+
+def prueba_1():
+    batallas = []
+
+    orden_esperado = []
+    suma_esperada = 0
+
+    orden, suma = organizar_batallas(batallas)
+
+    assert orden == orden_esperado, f"\nOrden obtenido:{orden} \nOrden esperado: {orden_esperado}"
+    assert suma == suma_esperada, f"\nSuma obtenida:{suma} \nSuma esperada: {suma_esperada}"
+
+def prueba_2():
+    batallas = [(3, 4)]
+
+    orden_esperado = [(3, 4)]
+    suma_esperada = 12
+
+    orden, suma = organizar_batallas(batallas)
+
+    assert orden == orden_esperado, f"\nOrden obtenido:{orden} \nOrden esperado: {orden_esperado}"
+    assert suma == suma_esperada, f"\nSuma obtenida:{suma} \nSuma esperada: {suma_esperada}"
+
+def prueba_3():
+    batallas = [
+        (3, 4),
+        (2, 2),
+        (1, 6),
+    ]
+
+    orden_esperado = [
+        (1, 6),
+        (3, 4),
+        (2, 2),
+    ]
+    suma_esperada = 34
+
+    orden, suma = organizar_batallas(batallas)
+
+    assert orden == orden_esperado, f"\nOrden obtenido:{orden} \nOrden esperado: {orden_esperado}"
+    assert suma == suma_esperada, f"\nSuma obtenida:{suma} \nSuma esperada: {suma_esperada}"
+
+def prueba_4():
+    batallas = [
+        (2, 3),
+        (2, 2),
+        (2, 1),
+    ]
+
+    orden_esperado = [
+        (2, 3),
+        (2, 2),
+        (2, 1),
+    ]
+    suma_esperada = 20
+
+    orden, suma = organizar_batallas(batallas)
+
+    assert orden == orden_esperado, f"\nOrden obtenido:{orden} \nOrden esperado: {orden_esperado}"
+    assert suma == suma_esperada, f"\nSuma obtenida:{suma} \nSuma esperada: {suma_esperada}"    
+
+if __name__ == "__main__":
+    prueba_1()
+    prueba_2()
+    prueba_3()
+    prueba_4()
+    print("Todas las pruebas pasaron")
